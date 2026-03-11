e4105cc512e5701e27ac416bfdeedbff81aca0cb
great01-cyber <greatujah088@gmail.com>
2026-03-11 13:11:15 +0000

    reset admin password and verify email

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -9,11 +9,10 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Ujah John',
-            'email' => 'greatujah088@gmail.com',
-            'password' => bcrypt('Cspdgx9y'),
-            'email_verified_at' => now(),
-        ]);
+        User::where('email', 'greatujah088@gmail.com')
+            ->update([
+                'email_verified_at' => now(),
+                'password' => bcrypt('Cspdgx9y'),
+            ]);
     }
 }
